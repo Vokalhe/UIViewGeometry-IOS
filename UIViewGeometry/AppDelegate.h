a6a2d136be570480591e6bbf7cfac65d2d1e7799
Admin <admin@mac-admin.local>
2016-08-31 22:27:52 +0300

    Initial Commit

---
+++ UIViewGeometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewGeometry
+//
+//  Created by Admin on 31.08.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewGeometry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewGeometry
+//
+//  Created by Admin on 31.08.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
